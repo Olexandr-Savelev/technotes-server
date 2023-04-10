49da32199851503bf6878c4804b1ed5c17637c57
Olexandr-Savelev <a.savelev.krm@gmail.com>
2023-04-11 02:21:39 +0300

    add mongoose sequence plugin to Note model

--- src/models/Note.ts
+++ src/models/Note.ts
@@ -1,23 +1,37 @@
 import mongoose from "mongoose";
+import mongooseSequence from "mongoose-sequence";
 
-const NoteModel = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "User",
+const AutoIncrement = mongooseSequence(mongoose as any) as any;
+
+const NoteModel = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: [true, "Please enter a password."],
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    complete: {
+      type: Boolean,
+      default: false,
+    },
   },
-  title: {
-    type: String,
-    required: [true, "Please enter a password."],
-  },
-  text: {
-    type: String,
-    required: true,
-  },
-  complete: {
-    type: Boolean,
-    default: false,
-  },
+  {
+    timestamps: true,
+  }
+);
+
+NoteModel.plugin(AutoIncrement, {
+  inc_field: "ticket",
+  id: "ticketNums",
+  start_seq: 500,
 });
 
 export default mongoose.model("Note", NoteModel);
